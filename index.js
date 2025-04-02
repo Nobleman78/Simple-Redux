db97f358f9c9b6578bb45f3efd9a7432a8f2a42c
Jesmin Chakma <jesminchakma39@gmail.com>
2025-04-02 14:27:50 +0600

    Reducer and store subscribe and store dispatch

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const {createStore}  = require("redux");
+
 // Defining Constant
 const ADD_USER = 'ADD_USER'
 // Initial State Declaration
 const initialUserState = {
     users: [
-        { name: 'Jesmin Chakma' }
+        { name: 'Jesmin Chakma',ID:1 }
     ]
 }
 // Action - what we are going to 
@@ -14,19 +16,33 @@ const initialUserState = {
 const addUser = () => {
     return {
         type: ADD_USER,
-        payload: { name: 'arkit' }
+        payload: [{ name: 'arkit', ID:2 },{name:'Nikal',ID:3}]
+            
+        
     }
 }
-const userReducer = (state = initialUserState, action) => {
+const userReducer = (state=initialUserState,action)=>{
     switch (action.type) {
         case ADD_USER:
-            return {
+            return{
                 ...state,
-                users: [...state.users, action.payload]
+                users:[...state.users,action.payload]
             }
-
+        
         default:
             return state;
     }
-
 }
+
+// Store meaning hold the state
+// 1.getState(), 2.dispatch(), 3.subscribe(),
+
+
+//Create Store
+const store = createStore(userReducer)
+store.subscribe(()=>{
+    console.log(JSON.stringify(store.getState(),null,2))
+})
+// Dispatch 
+store.dispatch(addUser())
+
